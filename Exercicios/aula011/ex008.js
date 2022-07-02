12188188dd2a2c6217cae391f4a38e12b7cf6f1a
Murilo Santori Boff <murilosantori45@gmail.com>
2022-07-02 12:25:46 -0300

    Condicions p1

---
+++ Exercicios/aula011/ex008.js
@@ -0,0 +1,4 @@
+var vel = 68.9
+if (vel > 60) {
+    console.log('Acima da velocidade permitida')
+}
---
+++ Exercicios/aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+if (pais == 'Brasil') {
+    console.log('Voces são brasileiros')
+}
+else {
+    console.log('Estrangeiro')
+}
