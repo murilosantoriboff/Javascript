1b23220387ef36cc1c4366ca1b4fbdb0d6ba45d4
Murilo Santori Boff <murilosantori45@gmail.com>
2022-07-03 07:41:25 -0300

    Conditions p2

---
+++ Exercicios/aula012/ex011.js
@@ -0,0 +1,10 @@
+var idade = 10
+if (idade > 18) {
+    console.log('Voçê é de MAIOR')
+} else {
+    if (idade < 18) {
+        console.log('Voçê é de MENOR')
+    }
+}
+
+/* Posso utilizar o ( else if ) */
