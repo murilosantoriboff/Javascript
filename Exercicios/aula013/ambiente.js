6576f6ed3a841526908042408dd8ea10c741d8fb
Murilo Santori Boff <murilosantori45@gmail.com>
2022-07-07 15:32:46 -0300

    repetições p1

---
+++ Exercicios/aula013/ambiente.js
@@ -0,0 +1,21 @@
+var cont = 1
+do {    
+    console.log(`Passo ${cont}`)
+    cont += 1
+} while (cont <= 7)
+
+/* 
+var cont = 1
+while (cont <= 7) {
+    console.log(`Passo ${cont}`)
+    cont += 1
+}
+*/
+
+/* console.log('Oi, tudo bem')
+console.log('Oi, tudo bem')
+console.log('Oi, tudo bem')
+console.log('Oi, tudo bem')
+console.log('Oi, tudo bem')
+console.log('Oi, tudo bem')
+console.log('Oi, tudo bem') */
